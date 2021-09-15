d0b6ad0e3f0c0268271ee719f809bf5b0ca35f6a
Sushmab98 <87186873+Sushmab98@users.noreply.github.com>
2021-09-15 17:25:15 +0530

    Create librarian.sql

---
+++ librarian.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `librarian` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
